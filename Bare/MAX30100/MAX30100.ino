f75d86c86f24276275ecc9cb65d17cc945eb8388
bert <bert.timmerman@xs4all.nl>
2017-12-05 23:47:32 +0100

    Updated MAX30100.ino.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- Bare/MAX30100/MAX30100.ino
+++ Bare/MAX30100/MAX30100.ino
@@ -1,61 +1,74 @@
-/*
-Arduino-MAX30100 oximetry / heart rate integrated sensor library
-Copyright (C) 2016  OXullo Intersecans <x@brainrapers.org>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+/*!
+ * \file Bare/MAX30100/MAX30100.ino
+ *
+ * \brief Arduino-MAX30100 oximetry / heart rate integrated sensor
+ * library.
+ *
+ * PulseOximeter is the higher level interface to the sensor it offers:
+ * <ul>
+ * <li>beat detection reporting,
+ * <li>heart rate calculation,
+ * <li>SpO2 (oxidation level) calculation.
+ * </ul>
+ *
+ * \author Copyright (C) 2016  OXullo Intersecans <x@brainrapers.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <Wire.h>
 #include "MAX30100_PulseOximeter.h"
 
 #define REPORTING_PERIOD_MS 1000
 
-// PulseOximeter is the higher level interface to the sensor
-// it offers:
-//  * beat detection reporting
-//  * heart rate calculation
-//  * SpO2 (oxidation level) calculation
-
 PulseOximeter pox;
 
 uint32_t tsLastReport = 0;
 
-// Callback (registered below) fired when a pulse is detected
+/*!
+ * \brief Callback (registered below) fired when a pulse is detected.
+ */
 void
 onBeatDetected ()
 {
     Serial.println ("Beat!");
 }
 
-void setup ()
+void
+setup ()
 {
     Serial.begin (115200);
 
     Serial.println ("Initializing MAX30100");
-    // Initialize the PulseOximeter instance and register a beat-detected callback
+    /* Initialize the PulseOximeter instance and register a
+     * beat-detected callback.
+     */
     pox.begin ();
     pox.setOnBeatDetectedCallback (onBeatDetected);
 }
 
-void loop ()
+void
+loop ()
 {
-    // Make sure to call update as fast as possible
+    /* Make sure to call update as fast as possible. */
     pox.update ();
 
-    // Asynchronously dump heart rate and oxidation levels to the serial
-    // For both, a value of 0 means "invalid"
-    if (millis () - tsLastReport > REPORTING_PERIOD_MS) {
+    /* Asynchronously dump heart rate and oxidation levels to the serial.
+     * For both, a value of 0 means "invalid".
+     */
+    if (millis () - tsLastReport > REPORTING_PERIOD_MS)
+    {
         Serial.print ("Heart rate:");
         Serial.print (pox.getHeartRate());
         Serial.print ("bpm / SpO2:");
